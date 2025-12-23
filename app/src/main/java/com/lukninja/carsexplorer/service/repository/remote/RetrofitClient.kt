20f312749e5952ac6539795113d98aac4a6c6d9b
Lucas <lucassilvasouto@gmail.com>
2025-12-23 17:12:10 -0300

    Configurar Retrofit

--- app/src/main/java/com/lukninja/carsexplorer/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/lukninja/carsexplorer/service/repository/remote/RetrofitClient.kt
@@ -1,4 +1,33 @@
 package com.lukninja.carsexplorer.service.repository.remote
 
-class RetrofitClient {
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient private constructor() {
+    companion object {
+        private lateinit var retrofit: Retrofit
+
+        private fun getRetrofitInstance(): Retrofit {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+
+            val httpClient = OkHttpClient.Builder().addInterceptor(logging).build()
+
+            if (!::retrofit.isInitialized) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl("https://api.api-ninjas.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient)
+                    .build()
+            }
+
+            return retrofit
+        }
+
+        fun <S> createService(serviceClass: Class<S>): S {
+            return getRetrofitInstance().create(serviceClass)
+        }
+    }
 }
