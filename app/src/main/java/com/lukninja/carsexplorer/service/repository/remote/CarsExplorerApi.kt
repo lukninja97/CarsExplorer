4afd2601ad2e941b32b41f7d9333023fa5ca1f6b
Lucas <lucassilvasouto@gmail.com>
2026-03-13 12:34:30 -0300

    Add URLs and endpoints of API to constants

--- app/src/main/java/com/lukninja/carsexplorer/service/repository/remote/CarsExplorerApi.kt
+++ app/src/main/java/com/lukninja/carsexplorer/service/repository/remote/CarsExplorerApi.kt
@@ -3,19 +3,22 @@ package com.lukninja.carsexplorer.service.repository.remote
 import com.lukninja.carsexplorer.service.model.dto.MakesDto
 import com.lukninja.carsexplorer.service.model.dto.ManufacturesDto
 import com.lukninja.carsexplorer.service.model.dto.ModelsDto
+import com.lukninja.carsexplorer.util.Constants.ENDPOINT_GET_MAKES_OF_CARS
+import com.lukninja.carsexplorer.util.Constants.ENDPOINT_GET_MODELS_PER_MAKE
+import com.lukninja.carsexplorer.util.Constants.ENDPOINT_GET_MANUFACTURERS
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CarsExplorerApi {
 
-    @GET("api/vehicles/GetMakesForVehicleType/car?format=json")
+    @GET(ENDPOINT_GET_MAKES_OF_CARS)
     suspend fun getMakesForVehicleType(): Response<MakesDto>
 
-    @GET("api/vehicles/getManufacturerDetails/{make}?format=json")
+    @GET(ENDPOINT_GET_MANUFACTURERS)
     suspend fun getManufactures(@Path("make") make: String): Response<ManufacturesDto>
 
-    @GET("api/vehicles/getmodelsformake/{make}?format=json")
+    @GET(ENDPOINT_GET_MODELS_PER_MAKE)
     suspend fun getModels(@Path("make") make: String): Response<ModelsDto>
 
 }
